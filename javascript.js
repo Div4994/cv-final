2c5c9d8577f4c3a2fd00b0df9886622682abdf24
Div4994 <139262229+Div4994@users.noreply.github.com>
2023-08-08 14:13:09 +0200

    first commit

---
+++ javascript.js
@@ -0,0 +1,18 @@
+javascript
+let dev ={
+    nom:"Vita"
+    prenom:"Divaine"
+    age:17
+    adresse: {
+        ville:"Mulhouse",
+        nomRue:"illzach",
+        numeroRue: 62,
+        codePostal:"68200"
+    }
+    competences: ["HTML","CSS","Javascript"]
+}
+console.log(dev.prenom);
+
+console.log(dev.adresse.codePostal);
+
+console.log(dev.competences[0])
